9af260bc65897306d85144ea8a1ce6749df08bc3
Anand Kannan <kannanenator.gmail.com>
2016-10-27 23:08:44 -0400

    add timing faculties

--- src/network.py
+++ src/network.py
@@ -12,6 +12,8 @@ and omits many desirable features.
 #### Libraries
 # Standard library
 import random
+from contextlib import contextmanager
+import time
 
 # Third-party libraries
 import numpy as np
@@ -53,18 +55,39 @@ class Network(object):
         tracking progress, but slows things down substantially."""
         if test_data: n_test = len(test_data)
         n = len(training_data)
+
         for j in xrange(epochs):
-            random.shuffle(training_data)
-            mini_batches = [
-                training_data[k:k+mini_batch_size]
-                for k in xrange(0, n, mini_batch_size)]
-            for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
-            if test_data:
-                print "Epoch {0}: {1} / {2}".format(
-                    j, self.evaluate(test_data), n_test)
-            else:
-                print "Epoch {0} complete".format(j)
+            with(context_timer("Epoch {0}".format(j))):
+                random.shuffle(training_data)
+                mini_batches = [training_data[k:k+mini_batch_size]
+                    for k in xrange(0, n, mini_batch_size)]
+                for mini_batch in mini_batches:
+                    self.update_mini_batch(mini_batch, eta)
+                if test_data:
+                    print "Epoch {0}: {1} / {2}".format(
+                        j, self.evaluate(test_data), n_test)
+                else:
+                    print "Epoch {0} complete".format(j)
+            
+    def update_mini_batch_matrix(self, mini_batch, eta):
+        # these get updated on the average of all the gradients
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        # transform mini_batch into a 2d array and get x and y chunks
+        x, y = mini_batch[:,(0,-1)], mini_batch[:,-1]
+        delta_nabla_b, delta_nabla_w = self.backprop_matrix(x, y) # matrix backprop
+
+        # add all the gradients at once
+        nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)] 
+        nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+
+        # same weight/bias updates
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
+
 
     def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying
@@ -73,6 +96,9 @@ class Network(object):
         is the learning rate."""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        # here is where we have to refactor to use a matrix approach
+        
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
@@ -81,6 +107,28 @@ class Network(object):
                         for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b-(eta/len(mini_batch))*nb
                        for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop_matrix(self, x, y):
+        """ Backprop in matrix format """
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        activations = [x] #input activations for all training examples
+        zs = []
+        
+        activation = x
+
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, np.transpose(activation)) + b
+            activation = sigmoid(z)
+            zs.append(z)
+            activations.append(activation)
+
+        delta = self.cost_derivative(activations[-1], y) *\
+                sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        #nabla_w[-1] = 
+
 
     def backprop(self, x, y):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
@@ -139,3 +187,11 @@ def sigmoid(z):
 def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
+
+@contextmanager
+def context_timer(name):
+    """timer instead of timeit"""
+    startTime = time.time()
+    yield
+    elapsedTime = time.time() - startTime
+    print "{0} finished in {1} s".format(name, int(elapsedTime))
